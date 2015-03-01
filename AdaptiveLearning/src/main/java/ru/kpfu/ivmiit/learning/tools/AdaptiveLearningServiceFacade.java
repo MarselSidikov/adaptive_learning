1f019131f6b457dbf2cecb36278d1756ec567cfe
Irina Shakhova <irinoise@gmail.com>
2015-03-01 15:12:45 +0300

    add service interface

---
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/AdaptiveLearningServiceFacade.java
@@ -0,0 +1,23 @@
+package ru.kpfu.ivmiit.learning.tools;
+
+/**
+ * @author Sidikov Marsel (Kazan Federal University)
+ *
+ */
+public class AdaptiveLearningServiceFacade {
+    String login(LoginData data);
+
+    boolean checkLogin(String login);
+
+    void logout(String userToken);
+
+    String signUp(User user);
+
+    User getProfile(String userToken);
+
+    Test getTest(String userToken);
+
+    void answersSubmit(String userToken, Answers answers);
+
+    
+}
