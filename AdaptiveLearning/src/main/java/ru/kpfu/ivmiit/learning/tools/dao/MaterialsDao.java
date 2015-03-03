75ca4c29ee807e28e0bee6c60312e2311cacca7a
Zulfat Miftahutdinov <zulfatmi@gmail.com>
2015-03-03 15:16:38 +0300

    InterfaceDao intermediate

--- AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/MaterialsDao.java
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/MaterialsDao.java
@@ -1,8 +1,15 @@
 package ru.kpfu.ivmiit.learning.tools.dao;
 
+import ru.kpfu.ivmiit.learning.tools.models.Test;
+
+import java.util.Collection;
+
 /**
  * @author Sidikov Marsel (Kazan Federal University)
  *
  */
 public interface MaterialsDao {
+    String getMaterialText();
+    Collection<Test> getRelativeTests();
+    int getComplexity();
 }
--- AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/UsersDao.java
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/UsersDao.java
@@ -1,8 +1,14 @@
 package ru.kpfu.ivmiit.learning.tools.dao;
+import java.util.Collection;
 
 /**
  * @author Sidikov Marsel (Kazan Federal University)
  *
  */
-public interface UsersDao {
+public interface UsersDao<TestResults> {
+    Collection<TestResults> getTestsResults();
+    String getUserName ();
+    String getUserSurname();
+    void insertTestResults (int testId);
+    Collection<MaterialsDao> getCurrentMaterials();
 }
