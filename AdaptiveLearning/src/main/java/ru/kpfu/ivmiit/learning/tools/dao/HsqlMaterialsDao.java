d20aa0929521b08b7dfd92e362722b37648f823e
MarselSidikov <sidikov.marsel@gmail.com>
2015-03-04 00:49:13 +0300

    some fixed

--- AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/HsqlMaterialsDao.java
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/HsqlMaterialsDao.java
@@ -1,7 +1,25 @@
 package ru.kpfu.ivmiit.learning.tools.dao;
 
+import ru.kpfu.ivmiit.learning.tools.models.Test;
+
+import java.util.Collection;
+
 /**
  * @author Marsel Sidikov (Kazan Federal University)
  */
 public class HsqlMaterialsDao implements MaterialsDao {
+    @Override
+    public String getMaterialText() {
+        return null;
+    }
+
+    @Override
+    public Collection<Test> getRelativeTests() {
+        return null;
+    }
+
+    @Override
+    public int getComplexity() {
+        return 0;
+    }
 }
