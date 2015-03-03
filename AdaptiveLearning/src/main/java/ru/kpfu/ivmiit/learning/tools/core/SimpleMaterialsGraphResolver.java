8c598e43cbf1089b4360c8f1b85e14e010b54d9f
Zulfat Miftahutdinov <zulfatmi@gmail.com>
2015-03-03 16:11:56 +0300

    add architecture

--- AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/core/SimpleMaterialsGraphResolver.java
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/core/SimpleMaterialsGraphResolver.java
@@ -1,8 +1,11 @@
 package ru.kpfu.ivmiit.learning.tools.core;
 
+import ru.kpfu.ivmiit.learning.tools.dao.MaterialsDao;
+
 /**
  * @author Sidikov Marsel (Kazan Federal University)
  *
  */
 public class SimpleMaterialsGraphResolver implements MaterialsResolver {
+    MaterialsDao materialsDao;
 }
