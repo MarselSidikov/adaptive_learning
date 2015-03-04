62a34a345c08981da98a024093d737cd9651e3e4
MarselSidikov <sidikov.marsel@gmail.com>
2015-03-04 13:07:21 +0300

    simple implements added

--- AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/core/TestProviderImpl.java
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/core/TestProviderImpl.java
@@ -1,7 +1,21 @@
 package ru.kpfu.ivmiit.learning.tools.core;
 
+import ru.kpfu.ivmiit.learning.tools.models.Answers;
+import ru.kpfu.ivmiit.learning.tools.models.Test;
+
+import java.util.List;
+
 /**
  * @author Marsel Sidikov (Kazan Federal University)
  */
 public class TestProviderImpl implements TestProvider {
+	@Override
+	public Test getTest(List<Integer> results, int materialId) {
+		return null;
+	}
+
+	@Override
+	public int getResult(String userToken, Answers answers) {
+		return 0;
+	}
 }
