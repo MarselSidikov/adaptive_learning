0ed5bd98a01a56256da8d23bf0f8d0abf9b177d4
Zulfat Miftahutdinov <zulfatmi@gmail.com>
2015-03-04 09:09:33 +0300

    DAO_INterface added javadoc to MatrialsDAO

--- AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/MaterialsDao.java
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/MaterialsDao.java
@@ -1,5 +1,6 @@
 package ru.kpfu.ivmiit.learning.tools.dao;
 
+import ru.kpfu.ivmiit.learning.tools.models.Material;
 import ru.kpfu.ivmiit.learning.tools.models.Test;
 
 import java.util.Collection;
@@ -9,7 +10,19 @@ import java.util.Collection;
  *
  */
 public interface MaterialsDao {
-    String getMaterialText();
-    Collection<Test> getRelativeTests();
-    int getComplexity();
+    /**
+     *
+     * @param id material id
+     * @throws java.lang.IllegalArgumentException if id is invalid
+     * @return material
+     */
+    Material getMaterial(int id);
+
+    /**
+     *
+     * @param id
+     * @throws java.lang.IllegalArgumentException if id is invalid
+     * @return alternativeMaterial for related theme
+     */
+    int getAlternativeMaterial(int id);
 }
