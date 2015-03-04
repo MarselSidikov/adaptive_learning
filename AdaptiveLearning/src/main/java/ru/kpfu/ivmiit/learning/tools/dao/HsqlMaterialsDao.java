6324ddbbbd387603e16f1e7cd8dcf46a4d993afc
MarselSidikov <sidikov.marsel@gmail.com>
2015-03-04 16:12:02 +0300

    some fixs

--- AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/HsqlMaterialsDao.java
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/HsqlMaterialsDao.java
@@ -1,5 +1,6 @@
 package ru.kpfu.ivmiit.learning.tools.dao;
 
+import ru.kpfu.ivmiit.learning.tools.models.Material;
 import ru.kpfu.ivmiit.learning.tools.models.Test;
 
 import java.util.Collection;
@@ -8,18 +9,14 @@ import java.util.Collection;
  * @author Marsel Sidikov (Kazan Federal University)
  */
 public class HsqlMaterialsDao implements MaterialsDao {
+
     @Override
-    public String getMaterialText() {
+    public Material getMaterial(int id) {
         return null;
     }
 
     @Override
-    public Collection<Test> getRelativeTests() {
-        return null;
-    }
-
-    @Override
-    public int getComplexity() {
+    public int getAlternativeMaterial(int id) {
         return 0;
     }
 }
