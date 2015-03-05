b5ef5998a53a1ab8497bdcd6c2942ca98ad27eef
Ilnar <ilnar3727@gmail.com>
2015-03-05 11:28:46 +0300

    Update LoginData.java

--- AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/models/LoginData.java
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/models/LoginData.java
@@ -1,6 +1,18 @@
 package ru.kpfu.ivmiit.learning.tools.models;
 
 public class LoginData {
-  String login;
-  String password;
+  private String login;
+  private String password;
+  public void setLogin(String login){
+    this.login = login;
+  }
+  public void setPassword(String password){
+    this.password = password;
+  }
+  public String getLogin(){
+    return login;
+  }
+  public String getPassword(){
+    return password;
+  }
 }
