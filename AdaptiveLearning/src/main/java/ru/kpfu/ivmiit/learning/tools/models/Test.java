0626e78e6cba530ef40aa065cd9aca0e42356f15
Ilnar <ilnar3727@gmail.com>
2015-03-05 11:33:11 +0300

    Update Test.java

--- AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/models/Test.java
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/models/Test.java
@@ -1,8 +1,14 @@
 package ru.kpfu.ivmiit.learning.tools.models;
 
 public class Test {
-  ArrayList<String> questions;
-  public Test(){
+  private ArrayList<String> questions;
+    public Test(){
     questions = new ArrayList<String>();
+  }
+  public void setQuestions(ArrayList<String> questions){
+    this.questions = questions;
+  }
+  public ArrayList<String> getQuestions(){
+    return questions;
   }
 }
