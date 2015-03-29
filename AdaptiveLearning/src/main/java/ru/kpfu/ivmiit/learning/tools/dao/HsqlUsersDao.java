719f992ebc3e816ecf66b5408e3678cdda980a73
IgorLebedenko <igor777lebedenko@yandex.ru>
2015-03-29 23:44:50 +0300

    some fixes

--- AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/HsqlUsersDao.java
+++ AdaptiveLearning/src/main/java/ru/kpfu/ivmiit/learning/tools/dao/HsqlUsersDao.java
@@ -4,7 +4,10 @@ import ru.kpfu.ivmiit.learning.tools.models.LoginData;
 import ru.kpfu.ivmiit.learning.tools.models.User;
 
 import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
+import java.security.SecureRandom;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Sidikov Marsel, Lebedenko Igor, Karpov Oleg (Kazan Federal University)
@@ -13,17 +16,33 @@ import java.util.List;
 public class HsqlUsersDao extends SimpleJdbcDaoSupport implements UsersDao {
     @Override
     public String login(LoginData data) {
-        String login = data.getLogin();
-        String password = data.getPassword();
+        Map<String, String> paramMap = new HashMap<String, String>();
 
-        String sql = "SELECT COUNT(*) FROM Student WHERE login = :login AND passwHash = :password";
-        int count = getSimpleJdbcTemplate().queryForInt(sql, login, password);
+        paramMap.put("login", data.getLogin());
+        paramMap.put("password", data.getPassword());
 
-        if (count == 0) {
+        String sql = "SELECT COUNT(*) FROM Student WHERE (login = :login AND passwHash = :password)";
+        int count = getSimpleJdbcTemplate().queryForInt(sql, paramMap);
+
+        if (count == 1) {
+            String AB = "0123456789abcdefghigklmnopqrstuvwxyz";
+            SecureRandom rnd = new SecureRandom();
+
+            StringBuilder sb = new StringBuilder(32);
+            for (int i = 0; i < 32; i++) {
+                sb.append(AB.charAt(rnd.nextInt(AB.length())));
+            }
+            String userToken = sb.toString();
+
+            paramMap.put("userToken", userToken);
+            sql = "UPDATE Student SET userToken = :userToken WHERE (login = :login AND passwHash = :password)";
+            getSimpleJdbcTemplate().update(sql, paramMap);
+
+            return userToken;
+        }
+        else {
             throw new IllegalArgumentException();
         }
-
-        return login;
     }
 
     @Override
@@ -35,15 +54,30 @@ public class HsqlUsersDao extends SimpleJdbcDaoSupport implements UsersDao {
         String sql = "SELECT COUNT(*) FROM Student WHERE login = :login";
         int count = getSimpleJdbcTemplate().queryForInt(sql, login);
 
-        if (count == 0) {
-            return true;
-        }
-
-        return false;
+        return count == 0;
     }
 
     @Override
     public void logout(String userToken) {
+        if (userToken == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Map<String, String> paramMap = new HashMap<String, String>();
+
+        paramMap.put("userToken", userToken);
+
+        String sql = "SELECT COUNT(*) FROM Student WHERE userToken = :userToken";
+        int count = getSimpleJdbcTemplate().queryForInt(sql, paramMap);
+
+        if (count == 1) {
+            paramMap.put("newUserToken", "");
+            sql = "UPDATE Student SET userToken = :newUserToken WHERE userToken = :userToken";
+            getSimpleJdbcTemplate().update(sql, paramMap);
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
